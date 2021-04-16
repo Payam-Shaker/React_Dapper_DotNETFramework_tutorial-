f95d9fdeee50d6c81412c47535b7b2caa6ab92d2
PayamS <72343921+Payam-Shaker@users.noreply.github.com>
2021-04-16 18:07:05 +0200

    React app and components created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {Home} from './components/Home'
+import {Department} from './components/Department'
+import {Employee} from './components/Employee'
+import {Navigation} from './components/Navigation'
+
+import {BrowserRouter, NavLink, Route, Switch} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+    <div className="container">
+
+    <h3 className="m-3 d-flex justify-content-center">
+      React JS with Web API Demo</h3> 
+    <h5 className="m-3 d-flex justify-content-center">
+      Employee Managment Portal</h5> 
+    <Navigation/>  
+    <Switch>
+    <Route path='/' component={Home} exact />
+    <Route path='/department' component={Department} />
+    <Route path='/employee' component={Employee} />
+    </Switch>
     </div>
+    </BrowserRouter>
   );
 }
-
 export default App;
---
+++ src/components/Department.js
@@ -0,0 +1,63 @@
+
+import React,{Component} from 'react';
+import {Table} from 'react-bootstrap';
+import {Button, ButtonToolbar} from 'react-bootstrap';
+import {AddDepModal} from './AddDepModal';
+export class Department extends Component{
+
+    constructor(props){
+        super(props);
+        this.state={deps:[], addModalShow : false}
+    }
+    componentDidMount(){
+        this.refreshList();
+    }
+
+    refreshList(){
+       fetch('https://localhost:44323/api/department')
+       .then(response=> response.json())
+       .then(data => {
+          this.setState({deps:data});
+       });
+    }
+    componentDidUpdate(){
+       this.refreshList(); 
+    }
+
+    render(){
+        const {deps} = this.state;
+        let addModalClose=() => this.setState({addModalShow:false}); 
+        return(
+            <div>
+            <Table className="mt-4" striped bordered hover size="sm">
+              <thead>
+                  <tr>
+                      <th>DepartmentID</th>
+                      <th>DepartmentName</th>
+                  </tr>
+              </thead>  
+              <tbody>
+                  {deps.map(dep=>
+                  <tr key={dep.DepartmentID}>
+                  <td>{dep.DepartmentID}</td>
+                  <td>{dep.DepartmentName}</td>
+                   </tr>
+                    )}
+              </tbody>
+            </Table>
+    <ButtonToolbar>
+            <Button
+            variant='primary'
+            onClick={()=> this.setState({addModalShow: true})}
+            >Add Department</Button>
+
+            <AddDepModal
+            show={this.state.addModalShow}
+            onHide={addModalClose}
+            />
+
+    </ButtonToolbar>
+    </div>        
+        )
+    }
+}
